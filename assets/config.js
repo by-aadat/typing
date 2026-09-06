3871fc083863b2e77affb3a808fc6df7bc683be9
by-aadat <ashishjio110@gmail.com>
2026-09-06 13:00:01 +0530

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -5,7 +5,7 @@
    Leave it empty to keep browsing the 100 sample records.
    --------------------------------------------------------------- */
 window.CONFIG = {
-  API_URL: "",              // e.g. "https://script.google.com/macros/s/AKfy...../exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbw4XFqjfPEVBaY1s1Rmnq0NXR68f1Zoa5X6lXagyOApthNt9WCAu8tfdof-_4mdjsJDYw/exec",              // e.g. "https://script.google.com/macros/s/AKfy...../exec"
   ADMIN_PIN: "2026",        // change this before you go live
   SCHOOL_YEAR: "2026 – 27"
 };
